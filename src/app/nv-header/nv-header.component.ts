b772770640fcec2d0cac9187ff6e83c277a9c741
Tiem Le <19522330@gm.uit.edu.vn>
2024-03-04 15:54:51 +0700

    narcissism voyage setup env

---
+++ src/app/nv-header/nv-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nv-header',
+  templateUrl: './nv-header.component.html',
+  styleUrls: ['./nv-header.component.scss']
+})
+export class NvHeaderComponent {
+  opened = false;
+}
---
+++ src/app/nv-home/components/nv-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nv-home',
+  templateUrl: './nv-home.component.html',
+  styleUrls: ['./nv-home.component.scss']
+})
+export class NvHomeComponent {
+
+}
